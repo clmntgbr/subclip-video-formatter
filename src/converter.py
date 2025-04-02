1b530abbccdd9e827843887337b4ff349f021b37
Clément <33451311+clmntgbr@users.noreply.github.com>
2025-04-02 18:19:49 +0200

    Add initial project structure with Docker, environment configuration, and core functionality
    - Introduced Dockerfile and docker-compose files for production and development environments.
    - Added .env and .gitignore files for environment variables and ignored files.
    - Implemented S3 and RabbitMQ client classes for file handling and messaging.
    - Created a Flask application with Celery for asynchronous task processing.
    - Included requirements.txt for project dependencies.
    - Added README.md for project description and features.

---
+++ src/converter.py
@@ -0,0 +1,91 @@
+import json
+from src.Protobuf.Message_pb2 import Clip, Video, Configuration
+
+
+class ProtobufConverter:
+    @staticmethod
+    def json_to_protobuf(message: str) -> Clip:
+        data = json.loads(message)
+
+        print(data)
+
+        clip_data = data["clip"]
+
+        clip = Clip()
+        clip.id = clip_data["id"]
+        clip.userId = clip_data["userId"]
+
+        if "cover" in clip_data:
+            clip.cover = clip_data["cover"]
+
+        video = Video()
+        video.id = clip_data["originalVideo"]["id"]
+        video.name = clip_data["originalVideo"]["name"]
+        video.originalName = clip_data["originalVideo"]["originalName"]
+        video.mimeType = clip_data["originalVideo"]["mimeType"]
+        video.size = int(clip_data["originalVideo"]["size"])
+
+        if "length" in clip_data["originalVideo"]:
+            video.length = int(clip_data["originalVideo"]["length"])
+
+        if "audios" in clip_data["originalVideo"]:
+            video.audios.extend(clip_data["originalVideo"]["audios"])
+
+        if "subtitles" in clip_data["originalVideo"]:
+            video.subtitles.extend(clip_data["originalVideo"]["subtitles"])
+
+        if "ass" in clip_data["originalVideo"]:
+            video.ass = clip_data["originalVideo"]["ass"]
+
+        if "subtitle" in clip_data["originalVideo"]:
+            video.subtitle = clip_data["originalVideo"]["subtitle"]
+
+        video.IsInitialized()
+        clip.originalVideo.CopyFrom(video)
+
+        if "processedVideo" in clip_data:
+            processed_video = Video()
+            processed_video.id = clip_data["processedVideo"]["id"]
+            processed_video.name = clip_data["processedVideo"]["name"]
+            processed_video.originalName = clip_data["processedVideo"]["originalName"]
+            processed_video.mimeType = clip_data["processedVideo"]["mimeType"]
+            processed_video.size = int(clip_data["processedVideo"]["size"])
+
+            if "length" in clip_data["processedVideo"]:
+                processed_video.length = int(clip_data["processedVideo"]["length"])
+
+            if "audios" in clip_data["processedVideo"]:
+                processed_video.audios.extend(clip_data["processedVideo"]["audios"])
+
+            if "subtitles" in clip_data["processedVideo"]:
+                processed_video.subtitles.extend(clip_data["processedVideo"]["subtitles"])
+
+            if "ass" in clip_data["processedVideo"]:
+                processed_video.ass = clip_data["processedVideo"]["ass"]
+
+            if "subtitle" in clip_data["processedVideo"]:
+                processed_video.subtitle = clip_data["processedVideo"]["subtitle"]
+            
+            processed_video.IsInitialized()
+            clip.processedVideo.CopyFrom(processed_video)
+
+        configuration = Configuration()
+        configuration.subtitleFont = clip_data["configuration"]["subtitleFont"]
+        configuration.subtitleSize = int(clip_data["configuration"]["subtitleSize"])
+        configuration.subtitleColor = clip_data["configuration"]["subtitleColor"]
+        configuration.subtitleBold = clip_data["configuration"]["subtitleBold"]
+        configuration.subtitleItalic = clip_data["configuration"]["subtitleItalic"]
+        configuration.subtitleUnderline = clip_data["configuration"]["subtitleUnderline"]
+        configuration.subtitleOutlineColor = clip_data["configuration"]["subtitleOutlineColor"]
+        configuration.subtitleOutlineThickness = int(clip_data["configuration"]["subtitleOutlineThickness"])
+        configuration.subtitleShadow = int(clip_data["configuration"]["subtitleShadow"])
+        configuration.subtitleShadowColor = clip_data["configuration"]["subtitleShadowColor"]
+        configuration.format = clip_data["configuration"]["format"]
+        configuration.split = int(clip_data["configuration"]["split"])
+
+        configuration.IsInitialized()
+        clip.configuration.CopyFrom(configuration)
+
+        clip.IsInitialized()
+
+        return clip
---
+++ src/main.py
@@ -0,0 +1,42 @@
+import os
+import re
+
+from kombu import Queue
+from flask import Flask
+from celery import Celery
+
+from src.config import Config
+from src.s3_client import S3Client
+from src.rabbitmq_client import RabbitMQClient
+from src.file_client import FileClient
+from src.converter import ProtobufConverter
+from src.Protobuf.Message_pb2 import Clip, ClipStatus, SubtitleTransformerMessage, Configuration, ConfigurationSubtitleFont
+
+app = Flask(__name__)
+app.config.from_object(Config)
+s3_client = S3Client(Config)
+rmq_client = RabbitMQClient()
+file_client = FileClient()
+
+celery = Celery("tasks", broker=app.config["RABBITMQ_URL"])
+
+celery.conf.update(
+    {
+        "task_serializer": "json",
+        "accept_content": ["json"],
+        "broker_connection_retry_on_startup": True,
+        "task_routes": {
+            "tasks.process_message": {"queue": app.config["RMQ_QUEUE_WRITE"]}
+        },
+        "task_queues": [
+            Queue(
+                app.config["RMQ_QUEUE_READ"], routing_key=app.config["RMQ_QUEUE_READ"]
+            )
+        ],
+    }
+)
+
+@celery.task(name="tasks.process_message", queue=app.config["RMQ_QUEUE_READ"])
+def process_message(message):
+    clip: Clip = ProtobufConverter.json_to_protobuf(message)
+    print(clip)
